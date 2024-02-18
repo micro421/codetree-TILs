39cbf4b8cab6ccb7c5ceef05aa7ec1852344037f
micro421 <125121156+micro421@users.noreply.github.com>
2024-02-18 22:33:54 +0900

    Title: 데이터센터의 온도 조정 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240218/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
+++ 240218/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -19,8 +19,8 @@ int main()
 	for (int i = 0; i < n; i++)
 	{
 		cin >> ta[i] >> tb[i];
-		min_degree = min(ta[i], tb[i]);
-		max_degree = max(ta[i], tb[i]);
+		min_degree = min(ta[i], min_degree);
+		max_degree = max(tb[i], max_degree);
 	}
 
 	int simul_one = 0;
@@ -28,26 +28,103 @@ int main()
 	int simul_three = 0;
 
 	int max_sum = 0;
-	for (int degree = min_degree - 1; degree <= max_degree + 1; degree++)
+	if (max_degree <= 999 &&min_degree>=1)
 	{
-		int sum = 0;
-		for (int i = 0; i < n; i++) // 특정 degree에 대해서 장비 1~N까지 다 돌려서 max 구해보기 이게 핵심 
-		{ // 그러니까 1대 n을 N번(장비 개수만큼 하기)
-			if (degree < ta[i])
-			{
-				/*sum += Simul_Func1(degree, ta[i], tb[i]); */
-				sum += c;
+		for (int degree = min_degree - 1; degree <= max_degree + 1; degree++)
+		{
+			int sum = 0;
+			for (int i = 0; i < n; i++) // 특정 degree에 대해서 장비 1~N까지 다 돌려서 max 구해보기 이게 핵심 
+			{ // 그러니까 1대 n을 N번(장비 개수만큼 하기)
+				if (degree < ta[i])
+				{
+					/*sum += Simul_Func1(degree, ta[i], tb[i]); */
+					sum += c;
+				}
+				if ((degree >= ta[i]) && (degree <= tb[i]))
+				{
+					sum += g;
+				}
+				if (degree > tb[i])
+				{
+					sum += h;
+				}
 			}
-			if ((degree >=ta[i]) && (degree<=tb[i]))
-			{
-				sum += g;
-			}
-			if (degree > tb[i])
-			{
-				sum += h;
-			}
+			max_sum = max(sum, max_sum);
 		}
-		max_sum = max(sum, max_sum);
+	}
+	if (max_degree == 1000 && min_degree >= 1)
+	{
+		for (int degree = min_degree - 1; degree <= 1000; degree++)
+		{
+			int sum = 0;
+			for (int i = 0; i < n; i++) // 특정 degree에 대해서 장비 1~N까지 다 돌려서 max 구해보기 이게 핵심 
+			{ // 그러니까 1대 n을 N번(장비 개수만큼 하기)
+				if (degree < ta[i])
+				{
+					/*sum += Simul_Func1(degree, ta[i], tb[i]); */
+					sum += c;
+				}
+				if ((degree >= ta[i]) && (degree <= tb[i]))
+				{
+					sum += g;
+				}
+				if (degree > tb[i])
+				{
+					sum += h;
+				}
+			}
+			max_sum = max(sum, max_sum);
+		}
+	}
+
+	if (min_degree == 0 && max_degree <= 999)
+	{
+		for (int degree= 0; degree <= max_degree + 1; degree++)
+		{
+			int sum = 0;
+			for (int i = 0; i < n; i++) // 특정 degree에 대해서 장비 1~N까지 다 돌려서 max 구해보기 이게 핵심 
+			{ // 그러니까 1대 n을 N번(장비 개수만큼 하기)
+				if (degree < ta[i])
+				{
+					/*sum += Simul_Func1(degree, ta[i], tb[i]); */
+					sum += c;
+				}
+				if ((degree >= ta[i]) && (degree <= tb[i]))
+				{
+					sum += g;
+				}
+				if (degree > tb[i])
+				{
+					sum += h;
+				}
+			}
+			max_sum = max(sum, max_sum);
+		}
+	}
+
+	if (min_degree == 0 && max_degree == 1000)
+	{
+		for (int degree = 0; degree <= 1000; degree++)
+		{
+			int sum = 0;
+			for (int i = 0; i < n; i++) // 특정 degree에 대해서 장비 1~N까지 다 돌려서 max 구해보기 이게 핵심 
+			{ // 그러니까 1대 n을 N번(장비 개수만큼 하기)
+				if (degree < ta[i])
+				{
+					/*sum += Simul_Func1(degree, ta[i], tb[i]); */
+					sum += c;
+				}
+				if ((degree >= ta[i]) && (degree <= tb[i]))
+				{
+					sum += g;
+				}
+				if (degree > tb[i])
+				{
+					sum += h;
+				}
+			}
+			max_sum = max(sum, max_sum);
+		}
 	}
 	cout << max_sum;
 
