70790e8c272b0782989268456d845acb041a71fd
micro421 <125121156+micro421@users.noreply.github.com>
2024-05-09 22:13:57 +0900

    Title: 1차원 폭발 게임, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240509/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240509/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -51,6 +51,7 @@ void Consecutive_Func(int len)
     int start_idx;
     new_bumb_flag = 0;
 
+    /* Compare 2EA*/
     for(int i=1; i<len; i++) {
         if (grid[i - 1] == grid[i])
         {
@@ -61,14 +62,13 @@ void Consecutive_Func(int len)
             consecutive_cnt = 1;
         }
 
-        if (consecutive_cnt >= m )
+        if (consecutive_cnt >= m ) /*Bumb Make Condition Ok*/
         {
             new_bumb_flag = 1;
             start_idx = i - m + 1; /*(m-1)+(i-m+1) = i */
-            Bumb_Fill(start_idx, grid[i], len);
+            Bumb_Fill(start_idx, grid[i], len); /*Bumb Make Condition => Bumb_Fill*/
         }
     }
-    ;
 }
 
 int main() {
@@ -81,22 +81,27 @@ int main() {
         cin >> grid[i];
     }
 
+    /*result 변수 재사용성 때문에*/
     result = n;
-  
+
+    /*Start Recursive*/
     while(1) {
-        Consecutive_Func(result);
-        if (new_bumb_flag == 1) {
+        Consecutive_Func(result); /* Only Compare 2EA*/
+        if (new_bumb_flag == 1) { /*New Bumb Occur!*/
             result = Bumb_Oper(result);
         }
         if (new_bumb_flag == 0) {
-            break;
+            break; /*If new bumb not Occur, break the infinite loop*/
         }
     }
-
+    /*exception of test case #2 as 제출기록*/
+    /*exception dealing because Consecutive_Func only compare 2EA >=result*/
     if(m == 1)
     {
         result = 0;
     }
+
+   // cout<<"new_bumb_flag is "<<new_bumb_flag<<"\n";
 
     cout<<result<<"\n";
     if(result>=1) {
