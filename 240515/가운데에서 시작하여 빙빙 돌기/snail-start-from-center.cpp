a59d2e8a888068ecea8b3f1f04d5c4b0eb66f04a
micro421 <125121156+micro421@users.noreply.github.com>
2024-05-15 19:04:32 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240515/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240515/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -7,36 +7,39 @@ int grid[100][100] = {0,0};
 int dx[4] = {0,-1,0,1};
 int dy[4] = {1,0,-1,0};
 
-bool InRange(int nx, int ny)
+bool InRange(int x, int y)
 {
-    return (0<=nx && nx<n && 0<=ny && ny<n);
+    return (0<=x && x<n && 0<=y && y<n);
 }
 int AdapatableRangeNum(int i)
 {
-    int range_num = 3;
-    for(int j=0; j<=5001; j++)
+    int dx_num;
+    for(int j=0; j<=50; j++)
     {
         int k1 = (2*j+1);
         int k2 = (2*j+3);
         if(i>=k1*k1 && i<k2*k2) /* AND OR 로직 실수 주의! */
         {
-            range_num = k2;
+            dx_num = j+2;
             break;
         }
     }
 
-    return range_num;
+    return dx_num;
 }
-
+/*range_num은 1부터 3^2-1까지는 2
+ * (2*j+1)^2부터 (2*j+3)^2-1은 j+2를 반환해줌
+ * 3^2부터 5^2-1까지는 3
+ * 5^2부터 7^2-1까지는 4
+ * 7^2부터 9^2-1까지는 5를 반환 해준다*/
 bool AdaptableFunc(int nx, int ny,int range_num)
 {
-    /*abs 쓰는 맨허튼 공식 말고 기준점 격자 인덱스 범위 공식!*/
-    return (nx<(n/2+range_num-1))&&(ny<(n/2+range_num-1)) &&(nx>(n/2-(range_num-1))) && (ny>(n/2-(range_num-1)));
+    return (abs(nx - n/2)<range_num) && (abs(ny- n/2)<range_num);
+    // wrong return (nx<(n/2+range_num))&&(ny<(n/2+range_num)) &&(nx>(n/2-(range_num))) && (ny>(n/2-(range_num)));
 }
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
 
     cin>>n;
     int dir_num = 0;
@@ -50,11 +53,10 @@ int main() {
         int nx = x+dx[dir_num];
         int ny = y+dy[dir_num];
         int range_num = AdapatableRangeNum(i);
-        // cout << "i is " << i << " and range_num is" << range_num << "\n";
+        //cout << "i is " << i << " and range_num is" << range_num << "\n";
 
         if(AdaptableFunc(nx,ny,range_num) && InRange(nx, ny) && (grid[nx][ny] == 0))
         {
-            /* https://www.codetree.ai/missions/5/problems/snail-number-square-2/explanation*/
             /* 미래에 쓰일 인덱스가 된다면 nx, ny를 x, y에 그대로 대입*/
             x = nx;
             y = ny;
@@ -62,7 +64,7 @@ int main() {
         else
         {
             dir_num = (dir_num + 1)%4;
-           // cout<<" rotate i is "<<i<<"\n";
+            // cout<<" rotate i is "<<i<<"\n";
             x = x+dx[dir_num];
             y = y+dy[dir_num];
         }
